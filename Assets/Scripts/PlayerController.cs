e39671cba56034f969e0435d9166a8489408b275
cinnamintRoll <96367858+cinnamintRoll@users.noreply.github.com>
2023-04-08 15:43:13 +0800

    v.0.3 Non-player objects prefabs have basic movement Objects are destroyed when they leave the screen Collisions between objects are handled appropriately Objects are spawned at the appropriate locations on time-based intervals

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    private float speed = 10.0f;
+    private float speed = 100.0f;
     private Rigidbody playerRb;
     private float zBound = 7;
 
@@ -39,6 +39,21 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
         }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Player has collided with enemy.");
+        }
+    }
 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
